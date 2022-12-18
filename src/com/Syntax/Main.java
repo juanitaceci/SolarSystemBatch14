9e4b3e1dbd30376355c221ba5f94abe8be7cd220
JuanitaCeci <juanitaceci0624@gmail.com>
2022-12-18 11:20:09 -0500

    The basic project Solar System is ready

--- src/com/Syntax/Main.java
+++ src/com/Syntax/Main.java
@@ -3,6 +3,10 @@ package com.Syntax;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("The Basic Structure is ready please write doewn your code in thos ");
+        SolarSystem Jinny =new SolarSystem();
+        Jinny.Sun=1;
+        Jinny.Moon=2;
+        Jinny.Planets=3;
+        
     }
 }
