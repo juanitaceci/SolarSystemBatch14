31d4a465372f99a40c70ab5bab7d7eb22a88626b
JuanitaCeci <juanitaceci0624@gmail.com>
2022-12-18 10:22:22 -0500

    Basic structue of the solar system

---
+++ src/com/Syntax/Main.java
@@ -0,0 +1,8 @@
+package com.Syntax;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("The Basic Structure is ready please write doewn your code in thos ");
+    }
+}
